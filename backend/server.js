d5ff93f5753387652da501985791bf4e53498c05
Asanka Wijesooriya <ramwijesooriya@gmail.com>
2025-06-25 18:30:19 +0530

    connect database

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+connectDB();
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
